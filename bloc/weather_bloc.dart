ac55da9017fae04567189149e0c835704da394bc
Prabal Das <prabaldas94059@gmail.com>
2019-10-27 19:55:39 +0530

    weather_bloc written code

--- bloc/weather_bloc.dart
+++ bloc/weather_bloc.dart
@@ -1,15 +1,35 @@
 import 'dart:async';
 import 'package:bloc/bloc.dart';
+import 'package:flutterxkrishno_2/models/models.dart';
+import 'package:flutterxkrishno_2/repositories/repositories.dart';
 import './bloc.dart';
 
+
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
-  @override
-  WeatherState get initialState => InitialWeatherState();
+  final WeatherRepository weatherRepository;
+
+  WeatherBloc( this.weatherRepository): assert(weatherRepository !=null);
+
+  
+
 
   @override
   Stream<WeatherState> mapEventToState(
     WeatherEvent event,
   ) async* {
-    // TODO: Add Logic
+  if (event is FetchWeather){
+    yield WeatherLoading();
+    try {
+      final Weather weather =await weatherRepository.getWeather(event.city);
+      yield WeatherLoaded(weather:weather);
+
+    }catch(_){
+      yield WeatherError();
+    }
   }
+  }
+
+  @override
+ 
+  WeatherState get initialState => WeatherEmpty();
 }
