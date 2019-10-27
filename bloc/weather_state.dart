0a0c79255d2e38c342c9f52c8d283d9f92a6d51d
Prabal Das <prabaldas94059@gmail.com>
2019-10-27 19:18:43 +0530

    updated weather_bloc

--- bloc/weather_state.dart
+++ bloc/weather_state.dart
@@ -1,10 +1,24 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutterxkrishno_2/models/models.dart';
+import 'package:meta/meta.dart';
+
 
 abstract class WeatherState extends Equatable {
-  const WeatherState();
-}
+  const WeatherState();}
+
 
 class InitialWeatherState extends WeatherState {
   @override
-  List<Object> get props => [];
+  List<Object> get props => [];}
+
+class WeatherEmpty extends WeatherState {}
+class WeatherLoading extends WeatherState {}
+class WeatherLoaded extends WeatherState {
+  final Weather weather;
+
+  const WeatherLoaded({@required this.weather}) : assert(weather != null);
+
+  @override
+  List<Object> get props => [weather];
 }
+class WeatherError extends WeatherState {}
